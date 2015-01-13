6bae0935b8ab15b47e07583ec1e3caa4579b3548
Mrunal Patel <mrunalp@gmail.com>
2015-01-12 22:29:26 -0500

    Update tests.

--- subnet_allocator_test.go
+++ subnet_allocator_test.go
@@ -5,9 +5,30 @@ import (
 )
 
 func TestAllocateSubnet(t *testing.T) {
-	sna, err := NewSubnetAllocator("10.1.2.0/24", 8, nil)
+	sna, err := NewSubnetAllocator("10.1.0.0/16", 8, nil)
 	if err != nil {
-		t.Fatal("Failed to initialize IP allocator: %v", err)
+		t.Fatal("Failed to initialize IP allocator: ", err)
 	}
-	t.Log(sna.GetNetwork())
+
+	sn, err := sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.0.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
+	sn, err = sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.1.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
+	sn, err = sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.2.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
 }
