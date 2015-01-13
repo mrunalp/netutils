cace45e9e8cfc010ba3a3e16e8fa2c580c717a6e
Mrunal Patel <mrunalp@gmail.com>
2015-01-12 22:44:23 -0500

    Tests for in use subnets.

--- subnet_allocator_test.go
+++ subnet_allocator_test.go
@@ -32,3 +32,26 @@ func TestAllocateSubnet(t *testing.T) {
 		t.Fatal("Did not get expected subnet")
 	}
 }
+
+func TestAllocateSubnetInUse(t *testing.T) {
+	inUse := []string{"10.1.0.0/24", "10.1.2.0/24", "10.2.2.2/24", "Invalid"}
+	sna, err := NewSubnetAllocator("10.1.0.0/16", 8, inUse)
+	if err != nil {
+		t.Fatal("Failed to initialize IP allocator: ", err)
+	}
+
+	sn, err := sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.1.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
+	sn, err = sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.3.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
+}
