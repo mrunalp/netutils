5fd0fefa7181f38e8b60dda854ac28316204227b
Mrunal Patel <mrunalp@gmail.com>
2015-01-12 22:29:10 -0500

    Adds equal capacity subnet allocator.

--- subnet_allocator.go
+++ subnet_allocator.go
@@ -7,11 +7,11 @@ import (
 
 type SubnetAllocator struct {
 	network  *net.IPNet
-	capacity int
+	capacity uint
 	allocMap map[string]bool
 }
 
-func NewSubnetAllocator(network string, capacity int, amap map[string]bool) (*SubnetAllocator, error) {
+func NewSubnetAllocator(network string, capacity uint, amap map[string]bool) (*SubnetAllocator, error) {
 	_, netIP, err := net.ParseCIDR(network)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to parse network address: %q", network)
@@ -19,17 +19,41 @@ func NewSubnetAllocator(network string, capacity int, amap map[string]bool) (*Su
 
 	netMaskSize, _ := netIP.Mask.Size()
 	fmt.Println(netMaskSize)
-	if capacity > (32 - netMaskSize) {
+	if capacity > (32 - uint(netMaskSize)) {
 		return nil, fmt.Errorf("Subnet capacity cannot be larger than number of networks available")
 	}
 
 	if amap == nil {
 		amap = make(map[string]bool)
 	}
-	return &SubnetAllocator{network: netIP, allocMap: amap}, nil
+	return &SubnetAllocator{network: netIP, capacity: capacity, allocMap: amap}, nil
 }
 
 func (sna *SubnetAllocator) GetNetwork() (*net.IPNet, error) {
-	fmt.Println(sna.network)
-	return sna.network, nil
+	var (
+		numSubnets    uint32
+		numSubnetBits uint
+	)
+	baseipu := IPToUint32(sna.network.IP)
+	netMaskSize, _ := sna.network.Mask.Size()
+	numSubnetBits = 32 - uint(netMaskSize) - sna.capacity
+	numSubnets = 1 << numSubnetBits
+
+	//fmt.Println(strconv.FormatUint(uint64(baseipu), 2))
+	var i uint32
+	for i = 0; i < numSubnets; i++ {
+		shifted := i << sna.capacity
+		if shifted == 10 {
+			break
+		}
+		ipu := baseipu | shifted
+		genIp := Uint32ToIP(ipu)
+		genSubnet := &net.IPNet{IP: genIp, Mask: net.CIDRMask(int(numSubnetBits)+netMaskSize, 32)}
+		if !sna.allocMap[genSubnet.String()] {
+			sna.allocMap[genSubnet.String()] = true
+			return genSubnet, nil
+		}
+	}
+
+	return nil, fmt.Errorf("No subnets available.")
 }
