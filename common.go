fd010980bc5a2719055f36cda5ae377a71b253e8
Mrunal Patel <mrunalp@gmail.com>
2015-01-15 17:41:30 -0500

    Add function to generate default gateway for a subnet.

--- common.go
+++ common.go
@@ -14,3 +14,9 @@ func Uint32ToIP(u uint32) net.IP {
 	binary.BigEndian.PutUint32(ip, u)
 	return net.IPv4(ip[0], ip[1], ip[2], ip[3])
 }
+
+// Generate the default gateway IP Address for a subnet
+func GenerateDefaultGateway(sna *net.IPNet) net.IP {
+	ip := sna.IP.To4()
+	return net.IPv4(ip[0], ip[1], ip[2], 1)
+}
