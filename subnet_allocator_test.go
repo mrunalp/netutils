5cc4dccee5a47c5af96f5ecc8f8f194efc9cefd5
Mrunal Patel <mrunalp@gmail.com>
2015-01-15 17:41:54 -0500

    Tests for default gateway.

--- subnet_allocator_test.go
+++ subnet_allocator_test.go
@@ -82,3 +82,24 @@ func TestAllocateReleaseSubnet(t *testing.T) {
 		t.Fatal("Did not get expected subnet")
 	}
 }
+
+func TestGenerateGateway(t *testing.T) {
+	sna, err := NewSubnetAllocator("10.1.0.0/16", 8, nil)
+	if err != nil {
+		t.Fatal("Failed to initialize IP allocator: ", err)
+	}
+
+	sn, err := sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.0.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
+
+	gatewayIP := GenerateDefaultGateway(sn)
+	t.Log(gatewayIP)
+	if gatewayIP.String() != "10.1.0.1" {
+		t.Fatal("Did not get expected gateway IP Address")
+	}
+}
