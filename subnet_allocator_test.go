e17f43f72828ebe6aa4f1041c9e7fb229412a658
Mrunal Patel <mrunalp@gmail.com>
2015-01-14 19:38:56 -0500

    Adds tests for subnet release.

--- subnet_allocator_test.go
+++ subnet_allocator_test.go
@@ -55,3 +55,30 @@ func TestAllocateSubnetInUse(t *testing.T) {
 		t.Fatal("Did not get expected subnet")
 	}
 }
+
+func TestAllocateReleaseSubnet(t *testing.T) {
+	sna, err := NewSubnetAllocator("10.1.0.0/16", 8, nil)
+	if err != nil {
+		t.Fatal("Failed to initialize IP allocator: ", err)
+	}
+
+	sn, err := sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.0.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
+
+	if err := sna.ReleaseNetwork(sn); err != nil {
+		t.Fatal("Failed to release the subnet")
+	}
+
+	sn, err = sna.GetNetwork()
+	if err != nil {
+		t.Fatal("Failed to get network: ", err)
+	}
+	if sn.String() != "10.1.0.0/24" {
+		t.Fatal("Did not get expected subnet")
+	}
+}
